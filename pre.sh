abe63d0fc06b9386abf3d4f025de2299cae424b5
刘坚 <liuyx1201@gmail.com>
2020-08-20 09:18:44 +0800

    2 commit

--- pre.sh
+++ pre.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-#host
+#添加主机名
 cat >> /etc/hosts <<EOF
-172.17.8.101  node1
-172.17.8.102  node2
-172.17.8.103  node3
+172.17.8.104  node1
+172.17.8.105  node2
+172.17.8.106  node3
 EOF
 
-#timezone
+#时间同步
 cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 timedatectl set-timezone Asia/Shanghai
 yum -y install ntpdate
 ntpdate cn.pool.ntp.org
 
-#firewalld
+#关闭防火墙
 systemctl stop firewalld
 systemctl disable firewalld
 
-#selinux
+#关闭selinux
 setenforce 0
 sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
 
-#swapoff
+#关闭swap
 swapoff -a
 sed -i '/swap/s/^/#/' /etc/fstab
 
-#sysctl.conf
+#修改sysctl.conf
 modprobe br_netfilter
 echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
 cat > /etc/sysctl.d/k8s.conf << EOF
@@ -36,19 +36,19 @@ vm.swappiness=0
 EOF
 sysctl -p /etc/sysctl.d/k8s.conf
 
-#ipvs
-#yum -y install ipset ipvsadm
-#
-#cat > /etc/sysconfig/modules/ipvs.modules <<EOF
-##!/bin/bash
-#modprobe -- ip_vs
-#modprobe -- ip_vs_rr
-#modprobe -- ip_vs_wrr
-#modprobe -- ip_vs_sh
-#modprobe -- nf_conntrack_ipv4
-#EOF
-#chmod 755 /etc/sysconfig/modules/ipvs.modules && \
-#bash /etc/sysconfig/modules/ipvs.modules && \
+#安装ipvs
+yum -y install ipset ipvsadm
+
+cat > /etc/sysconfig/modules/ipvs.modules <<EOF
+#!/bin/bash
+modprobe -- ip_vs
+modprobe -- ip_vs_rr
+modprobe -- ip_vs_wrr
+modprobe -- ip_vs_sh
+modprobe -- nf_conntrack_ipv4
+EOF
+chmod 755 /etc/sysconfig/modules/ipvs.modules && \
+bash /etc/sysconfig/modules/ipvs.modules && \
 #lsmod | grep -e ip_vs -e nf_conntrack_ipv4
 
 #Docker
@@ -73,7 +73,40 @@ yum install -y docker-ce-18.09.9-3.el7
 systemctl enable docker
 systemctl start docker
 
+#修改Docker Cgroup Driver为systemd
+cat > /etc/docker/daemon.json << EOF
+{
+    "exec-opts": ["native.cgroupdriver=systemd"],
+    "log-driver": "json-file",
+    "log-opts": {
+    "max-size": "100m"
+    },
+    "storage-driver": "overlay2",
+    "storage-opts": [
+        "overlay2.override_kernel_check=true"
+    ], 
+    "registry-mirrors": [
+        "https://kfwkfulq.mirror.aliyuncs.com",
+        "https://2lqq34jg.mirror.aliyuncs.com",
+        "https://pee6w651.mirror.aliyuncs.com",
+        "http://hub-mirror.c.163.com",
+        "https://docker.mirrors.ustc.edu.cn",
+        "https://registry.docker-cn.com"
+    ]
+}
+EOF
+#systemctl daemon-reload
+systemctl restart docker
 
-#Docker  cgroups change systemd
+yum install -y kubectl-1.15.3 kubeadm-1.15.3  kubelet-1.15.3
 
-yum install -y kubectl-1.18.6 kubeadm-1.18.6  kubelet-1.18.6
+#配置Kubernetes集群
+#node1初始化master
+#if [[ $1 -eq 4 ]]
+#  then
+#  kubeadm init --kubernetes-version=v1.15.3 --image-repository registry.cn-hangzhou.aliyuncs.com/google_containers --apiserver-advertise-address=172.17.8.104 --pod-network-cidr=10.244.0.0/16 --service-cidr=10.1.0.0/16 --ignore-preflight-errors=NumCPU >>/root/kubeadm.log
+#  join=`grep -E "kubeadm join.*--token.*" /root/kubeadm.log` 
+##其他node加入
+#else
+#  echo $join|bash
+#fi
